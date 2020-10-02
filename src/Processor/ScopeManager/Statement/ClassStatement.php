13da3ee41b69f57051487536792cbaa17e9dc3e4
David Rodrigues <david.proweb@gmail.com>
2020-10-02 15:09:06 -0300

    ClassStatement: implemented support to consts;

--- src/Processor/ScopeManager/Statement/ClassStatement.php
+++ src/Processor/ScopeManager/Statement/ClassStatement.php
@@ -13,13 +13,19 @@ class ClassStatement
     public static function apply(ScopeManager $scopeManager, ?Node $node): ?Node
     {
         if ($node instanceof Node\Stmt\Class_) {
-            self::applyStatements($scopeManager, $node->stmts, FunctionLikeStatement::class);
+            self::applyStatements($scopeManager, $node->stmts, ExpressionStatement::class);
         }
         else if ($node instanceof Node\Expr\New_) {
             $node->class = ExpressionStatement::apply($scopeManager, $node->class);
 
             foreach ($node->args as $arg) {
                 $arg->value = ExpressionStatement::apply($scopeManager, $arg->value);
+            }
+        }
+        else if ($node instanceof Node\Stmt\ClassConst ||
+                 $node instanceof Node\Stmt\Const_) {
+            foreach ($node->consts as $const) {
+                $const->value = ExpressionStatement::apply($scopeManager, $const->value);
             }
         }
 
