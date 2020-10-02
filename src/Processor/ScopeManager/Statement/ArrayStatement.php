0e18a7aca894d04d484dc121692b9fd82cc75d3a
David Rodrigues <david.proweb@gmail.com>
2020-10-02 15:29:06 -0300

    ArrayStatement: implemented support to yield and yield from;

--- src/Processor/ScopeManager/Statement/ArrayStatement.php
+++ src/Processor/ScopeManager/Statement/ArrayStatement.php
@@ -15,13 +15,17 @@ class ArrayStatement
         if ($node instanceof Node\Expr\ArrayDimFetch) {
             $node->dim = ExpressionStatement::apply($scopeManager, $node->dim);
         }
-        else if ($node instanceof Node\Expr\Array_||
+        else if ($node instanceof Node\Expr\Array_ ||
                  $node instanceof Node\Expr\List_) {
             $node->items = self::applyStatements($scopeManager, $node->items, ExpressionStatement::class);
         }
-        else if ($node instanceof Node\Expr\ArrayItem) {
+        else if ($node instanceof Node\Expr\ArrayItem ||
+                 $node instanceof Node\Expr\Yield_) {
             $node->key   = ExpressionStatement::apply($scopeManager, $node->key);
             $node->value = ExpressionStatement::apply($scopeManager, $node->value);
+        }
+        else if ($node instanceof Node\Expr\YieldFrom) {
+            $node->expr = ExpressionStatement::apply($scopeManager, $node->expr);
         }
 
         return null;
